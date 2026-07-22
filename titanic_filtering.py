3f8c4612117db8c87bde72baba5878c543f29025
Tayyab Latif <mlatif.bee21seecs@seecs.edu.pk>
2026-07-22 15:58:49 +0500

    Add filtering and analysis for Titanic dataset survivors and non-survivors

--- titanic_filtering.py
+++ titanic_filtering.py
@@ -16,3 +16,14 @@ print('rich women 1st class', len(rich_women))
 first_or_second = data[(data['pclass'] == 1) | (data['pclass'] == 2)]
 print('first or second class', len(first_or_second))
 
+#.loc[] takes two things separated by a comma: .loc[which_rows, which_columns]
+southern_ports = data[data['embark_town'].isin(['Southampton', 'Cherbourg'])]
+print("From Southampton/Cherbourg:", len(southern_ports))
+
+# .loc[] - filter rows AND pick specific columns
+survivors_names = data.loc[data['survived'] == 1, ['sex', 'age', 'class']]
+print(survivors_names.head())
+
+# Negation with ~
+non_survivors = data[~(data['survived'] == 1)]
+print("Non-survivors:", len(non_survivors))
